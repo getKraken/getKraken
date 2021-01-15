bef5db4d9d043130200eda3963e4a1ccb033d1ee
Yonatan Palagashvili <yonimotek@gmail.com>
2021-01-15 13:02:54 -0800

    added some serializers

--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+
 
 class User(models.Model):
   username = models.CharField(max_length=64)
@@ -7,4 +9,15 @@ class User(models.Model):
   def __str__(self):
     return self.username
 
-class Series ()
+  def get_absolute_url(self):
+    return reverse("user_detail", args=[str(self.id)])
+
+
+class Series(models.Model):
+  title = models.CharField(max_length=64)
+
+  def __str__(self):
+    return self.title
+
+  def get_absolute_url(self):
+    return reverse("series_details", args=[str(self.id)])
--- api/views.py
+++ api/views.py
@@ -1,7 +1,13 @@
 from rest_framework.generics import ListCreateAPIView,CreateAPIView
 from .serializers import UserSerializer
 
+from .models import User, Series
+from django.urls import reverse_lazy
+
+
 class CreateUserView(CreateAPIView):
   serializer_class = UserSerializer
 
+class CreateSeriesView(CreateAPIView):
+    serializer_class = 
   
