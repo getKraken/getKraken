94941694ff6a2c76960f1ca286b8497cf8a535c2
Yonatan Palagashvili <yonimotek@gmail.com>
2021-01-19 23:59:49 -0800

    recommit

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,15 @@
+function NavBar(props) {
+    return (
+        <header>
+          <ul>
+            <li> <a href='../pages/SeriesListPage'>Link to Series List Page <img> Kraken Logo </img> </a> </li>
+
+            <li> GetKraken title </li>
+
+            <li><a href='../pages/NewSeriesPage'>Link to New Series Page</a></li>
+
+            <li><a href='../pages/SignInLoginInPage'> Logout Button </a></li>
+          </ul>
+        </header>
+    );
+}
