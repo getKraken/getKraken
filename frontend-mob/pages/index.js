9e082267557ac574d29f5c9ccd7869d1e03ef779
JB-Tellez <jb.tellez@gmail.com>
2021-01-22 01:36:58 -0800

    adds themed landing page

--- frontend-mob/pages/index.js
+++ frontend-mob/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import LoginForm from '../forms/LoginForm'
+import LoginForm from '../forms/LoginAlt'
 
 export default function Home() {
   return (
